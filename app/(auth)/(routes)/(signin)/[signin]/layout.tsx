69b449b0d1da67c4fd5795a0b3abed7a724412ea
Milad Joodi <winbeta.com@gmail.com>
2024-05-18 00:52:34 +0330

    up

--- app/(auth)/(routes)/(signin)/[signin]/layout.tsx
+++
@@ -1,9 +0,0 @@
-const layout = () => {
-    return (
-        <div>
-            Enter
-        </div>
-    );
-}
-
-export default layout;
--- app/(auth)/(routes)/(signin)/[signin]/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-    return (
-        <div>
-            Enter
-        </div>
-    );
-}
-
-export default Page;
