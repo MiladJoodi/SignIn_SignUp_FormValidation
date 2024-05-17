36895b441fa524c6538eb72c81b9d24ea6dd1ed9
Milad Joodi <winbeta.com@gmail.com>
2024-05-18 00:45:28 +0330

    uo

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/about')) {
+    return NextResponse.rewrite(new URL('/about-2', request.url))
+  }
+ 
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+  }
+}
