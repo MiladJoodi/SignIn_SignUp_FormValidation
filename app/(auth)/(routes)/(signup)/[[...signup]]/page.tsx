ae08dbe2ccb9743500abace71ea2a914e3e885f2
Milad Joodi <winbeta.com@gmail.com>
2024-05-17 22:41:44 +0330

    up

--- app/(auth)/(routes)/(signup)/[[...signup]]/page.tsx
+++ app/(auth)/(routes)/(signup)/[[...signup]]/page.tsx
@@ -14,20 +14,59 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa6";
+import Link from "next/link";
 
 const signUpSchema = z.object({
   name: z
     .string()
-    .min(2, "Name Should have at least 2 characters.")
+    .min(2, "Name should have at least 2 characters.")
     .max(50, "Name should not exceed 50 characters")
     .refine(
       (value) => /^[a-zA-Z]+[-'s]?[a-zA-Z ]+$/.test(value),
-      "Name should contain only alphabets."
-    ),
+      "Name should contain only alphabets."),
+    email: z.string().email("Email must be valid"),
+    password: z.string().min(6, "Password should have at least 6 characters."),
+    confirmPassword: z.string().min(6, "Password should have at least 6 characters.")
+}).refine((data)=> data.password === data.confirmPassword, {
+    message: "Password does not match",
+    path: ["confirmPassword"]
 });
 
 const Page = () => {
-  return <div>page</div>;
+    const form = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+          name: "",
+          email:"",
+          password: "",
+          confirmPassword: "",
+        },
+      })
+
+      function onSubmit(values: z.infer<typeof signUpSchema>){
+        console.log(values)
+      }
+
+  return (
+    <>
+        <div className="signUpWrapper">
+            <div className="formWrapper">
+                {/* Left */}
+                <div className="left">
+                    <h3>Welcome Back!</h3>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <Link href={"/signin"}>
+                        <Button className="">
+                            
+                        </Button>
+                    </Link>
+                </div>
+
+                {/* Right */}
+            </div>
+        </div>
+    </>
+  );
 };
 
 export default Page;
