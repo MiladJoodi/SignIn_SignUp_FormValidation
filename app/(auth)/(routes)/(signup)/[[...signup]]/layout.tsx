1d1e26226884fbf92f5914f3afa56b02bfca5b47
Milad Joodi <winbeta.com@gmail.com>
2024-05-17 21:17:29 +0330

    Let's start 🎓

---
+++ app/(auth)/(routes)/(signup)/[[...signup]]/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+
+export const metadata: Metadata = {
+    title: 'Sign up | ShortlyURL',
+    description: 'Joodi.me'
+}
+
+const SignUpLayout = ({children} : {children: React.ReactNode}) =>{
+    return(
+        <>
+            <NextTopLoader color="000" showSpinner={false} />
+            {children}
+        </>
+    )
+}
+
+export default SignUpLayout;
